74d138b59367ceea16d19d6e99e56bf496a6f7da
Koi <4nozaki.ayumi@gmail.com>
2024-02-15 14:37:41 +1100

    Added two more cards, Dodge and Mercedes. Added CM to Inch conversion function.

--- autoProperties/autoProperties.go
+++ autoProperties/autoProperties.go
@@ -3,8 +3,11 @@ package AutoProperties
 type UnitType string
 
 const (
-	Inch UnitType = "inch"
-	CM   UnitType = "cm"
+	Inch          UnitType = "inch"
+	CM            UnitType = "cm"
+	BrandBWM      string   = "BMW"
+	BrandDodge             = "Dodge"
+	BrandMercedes          = "Mercedes"
 )
 
 type Unit struct {
@@ -12,6 +15,7 @@ type Unit struct {
 	T     UnitType
 }
 
+// Get function takes dimensions, displays them in Inches or CMs
 func (u Unit) Get(t UnitType) float64 {
 	value := u.Value
 
@@ -38,6 +42,7 @@ type Auto interface {
 	EnginePower() int
 }
 
+// DimensionsType structure
 type DimensionsType struct {
 	length Unit
 	width  Unit
@@ -47,13 +52,28 @@ type DimensionsType struct {
 func (d DimensionsType) Length() Unit { return d.length }
 func (d DimensionsType) Width() Unit  { return d.width }
 func (d DimensionsType) Height() Unit { return d.height }
+
+// NewUnitCM function to create auto dimension in CMs
 func NewUnitCM(value float64) Unit {
 	return Unit{Value: value, T: CM}
 }
+
+// NewUnitInch function to create auto dimension in Inches
 func NewUnitInch(value float64) Unit {
 	return Unit{Value: value, T: Inch}
 }
 
+//type DimensionsInch struct {
+//	length Unit
+//	width  Unit
+//	height Unit
+//}
+//
+//func (d DimensionsInch) Length() Unit { return d.length }
+//func (d DimensionsInch) Width() Unit  { return d.width }
+//func (d DimensionsInch) Height() Unit { return d.height }
+
+// baseAuto structure for other autos.
 type baseAuto struct {
 	model       string
 	maxSpeed    int
@@ -61,16 +81,60 @@ type baseAuto struct {
 	dimensions  Dimensions
 }
 
+// BMW structure template
 type BMW struct{ baseAuto }
 
 func (auto BMW) Dimensions() Dimensions { return auto.dimensions }
-func (auto BMW) Brand() string          { return "BMW" }
+func (auto BMW) Brand() string          { return BrandBWM }
 func (auto BMW) Model() string          { return auto.model }
 func (auto BMW) MaxSpeed() int          { return auto.maxSpeed }
 func (auto BMW) EnginePower() int       { return auto.enginePower }
 
-func NewBMW(model string, maxSpeed, enginePower int, height, width, length Unit) BMW {
+func NewBMW(model string, maxSpeed, enginePower int, length, width, height Unit) BMW {
 	return BMW{baseAuto{
+		model:       model,
+		maxSpeed:    maxSpeed,
+		enginePower: enginePower,
+		dimensions: DimensionsType{
+			height: height,
+			width:  width,
+			length: length,
+		},
+	}}
+}
+
+// Dodge structure template
+type Dodge struct{ baseAuto }
+
+func (auto Dodge) Dimensions() Dimensions { return auto.dimensions }
+func (auto Dodge) Brand() string          { return BrandDodge }
+func (auto Dodge) Model() string          { return auto.model }
+func (auto Dodge) MaxSpeed() int          { return auto.maxSpeed }
+func (auto Dodge) EnginePower() int       { return auto.enginePower }
+
+func NewDodge(model string, maxSpeed, enginePower int, height, width, length Unit) Dodge {
+	return Dodge{baseAuto{
+		model:       model,
+		maxSpeed:    maxSpeed,
+		enginePower: enginePower,
+		dimensions: DimensionsType{
+			height: height,
+			width:  width,
+			length: length,
+		},
+	}}
+}
+
+type Mercedes struct{ baseAuto }
+
+func (auto Mercedes) Dimensions() Dimensions { return auto.dimensions }
+func (auto Mercedes) Brand() string          { return BrandMercedes }
+func (auto Mercedes) Model() string          { return auto.model }
+func (auto Mercedes) MaxSpeed() int          { return auto.maxSpeed }
+func (auto Mercedes) EnginePower() int       { return auto.enginePower }
+
+func NewMercedes(model string, maxSpeed, enginePower int, height, width, length Unit) Mercedes {
+	return Mercedes{baseAuto{
 		model:       model,
 		maxSpeed:    maxSpeed,
 		enginePower: enginePower,
--- main.go
+++ main.go
@@ -8,16 +8,53 @@ import (
 func PrintAutoParams(a AutoProperties.Auto) {
 	fmt.Printf("Brand: %v\nModel: %v\nEnginePower: %v\nMaxSpeed: %v\n", a.Brand(), a.Model(), a.EnginePower(), a.MaxSpeed())
 	dim := a.Dimensions()
-	fmt.Printf("Dimensions: Length %v, Width %v, Height %v", dim.Length(), dim.Width(), dim.Height())
+	fmt.Printf("Dimensions: Length %v, Width %v, Height %v\n", dim.Length(), dim.Width(), dim.Height())
+}
+func DisplayInch(a AutoProperties.Auto) {
+	fmt.Printf("%v's dimensions:\n", a.Model())
+	fmt.Printf("Lenght: %.1f inch\t", a.Dimensions().Length().Get(AutoProperties.Inch))
+	fmt.Printf("Width: %.1f inch\t", a.Dimensions().Width().Get(AutoProperties.Inch))
+	fmt.Printf("Height: %.1f inch\n", a.Dimensions().Height().Get(AutoProperties.Inch))
 }
 func main() {
+
 	bmw1 := AutoProperties.NewBMW(
-		"RX7",
-		120,
-		40,
-		AutoProperties.NewUnitCM(100),
-		AutoProperties.NewUnitCM(80),
-		AutoProperties.NewUnitCM(150),
+		"M8 Competition Gran Coupe",
+		305,
+		617,
+		AutoProperties.NewUnitCM(510.286),
+		AutoProperties.NewUnitCM(194.31),
+		AutoProperties.NewUnitCM(141.986),
+	)
+	dodge1 := AutoProperties.NewDodge(
+		"Charger",
+		326,
+		807,
+		AutoProperties.NewUnitInch(198.4),
+		AutoProperties.NewUnitInch(75),
+		AutoProperties.NewUnitInch(57.8),
+	)
+	mercedes1 := AutoProperties.NewMercedes(
+		"AMG SL Roadster",
+		315,
+		585,
+		AutoProperties.NewUnitCM(407.5),
+		AutoProperties.NewUnitCM(191.5),
+		AutoProperties.NewUnitCM(135.9),
+	)
+	dodge2 := AutoProperties.NewDodge(
+		"Charger(CM)",
+		326,
+		807,
+		AutoProperties.NewUnitCM(503.9),
+		AutoProperties.NewUnitCM(190.5),
+		AutoProperties.NewUnitCM(146.8),
 	)
 	PrintAutoParams(bmw1)
+	PrintAutoParams(dodge1)
+	PrintAutoParams(mercedes1)
+	DisplayInch(dodge1)
+	DisplayInch(dodge2)
+	DisplayInch(bmw1)
+	DisplayInch(mercedes1)
 }
